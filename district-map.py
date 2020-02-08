e5d3ae22403dad4b4c421448dcfa3c49f849a242
vishwakulkarni <vishwa.kulkarni@gmail.com>
2020-02-07 23:54:16 -0700

    offence code to name

---
+++ district-map.py
@@ -0,0 +1,9 @@
+{
+    1:"City and County of Denver",
+    2:"northwestern suburbs of Denver",
+    3:"rural Western Slope",
+    4:"rural Eastern Plains",
+    5:"Colorado Springs",
+    6:"Denver-Aurora Metropolitan Area",
+    7:"western parts of the Denver-Aurora Metropolitan Area"
+}
